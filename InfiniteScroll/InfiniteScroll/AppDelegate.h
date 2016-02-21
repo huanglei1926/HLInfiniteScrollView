2a50d8555f0af3741c6719dfb73f1a79c175af2f
huanglei1926 <huanglei1926@gmail.com>
2016-02-21 21:31:29 +0800

    For the first time to create a project file
    无线滚动简易框架

---
+++ InfiniteScroll/InfiniteScroll/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InfiniteScroll
+//
+//  Created by line on 15/3/21.
+//  Copyright © 2015年 line. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InfiniteScroll/InfiniteScroll/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InfiniteScroll
+//
+//  Created by line on 15/3/21.
+//  Copyright © 2015年 line. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
