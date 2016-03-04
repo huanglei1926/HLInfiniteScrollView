ae82f290d65e6da27089b6a75962d2351b74ce16
huanglei1926 <huanglei1926@gmail.com>
2016-03-04 21:06:45 +0800

    Revert "Revert "For the first time to create a project file""
    This reverts commit 36fa15c67d040726668738267707250b7d01c1ef.

---
+++ InfiniteScroll/InfiniteScroll/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  InfiniteScroll
+//
+//  Created by line on 15/3/21.
+//  Copyright © 2015年 line. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
